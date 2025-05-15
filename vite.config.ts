2d919490a08ecb16285ee8d940aa2c44a8c2dacd
transitley <transitley@gmail.com>
2025-05-15 15:13:34 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
-// import preline from '@preline/plugin';  // Correct import path
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
+console.log('BUILD FROM:', __dirname);
 
 export default defineConfig({
-       plugins: [
-        tailwindcss(),
-        react(),
-      ],
- 
-  
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
 });
